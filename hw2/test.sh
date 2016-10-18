ad2a08dcae0eebfe5872ff26f0cb437af48b07a6
m516825 <b01902078@ntu.edu.tw>
2016-10-18 19:49:06 +0800

    remove

--- hw2/test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python ./logistic_regression.py --type 2 --model $1 --test_dat $2 --output_dat $3
--- hw2/train.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python ./logistic_regression.py --train_dat $1 --model $2
