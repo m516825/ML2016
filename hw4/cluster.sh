0581203ca7fa843c1e77632dd579488629f04111
m516825 <b01902078@ntu.edu.tw>
2016-11-27 22:36:46 +0800

    fix cluster.sh

--- hw4/cluster.sh
+++ hw4/cluster.sh
@@ -4,5 +4,5 @@ desh=${dir: -1}
 if [ "$desh" != "/" ]; then
 	dir="$1/"
 fi
-python2.7 word2vec_unsupervised.py --mode "test" --title_doc "$1title_StackOverflow.txt" --pred_pairs "$1check_index.csv" \
---content "$1docs.txt" --pred_out $2
+python2.7 word2vec_unsupervised.py --mode "test" --title_doc $dir"title_StackOverflow.txt" --pred_pairs $dir"check_index.csv" \
+--content $dir"docs.txt" --pred_out $2
